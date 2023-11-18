832d405d911167f74c5c6ec35ecd17fe94baf3ad
sydney andre <s.a.andre0217@gmail.com>
2023-11-18 15:07:08 -0600

    add/ gallery comp created

---
+++ client/components/Gallery.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+const Gallery = (props) => {
+  const { user, userEventsArr } = props;
+
+  // const getAllUserPhotos = () => {
+
+  // }
+  return (
+    <div>
+      {/* {console.log(userEventsArr)} */}
+      Gallery
+    </div>
+  );
+};
+
+export default Gallery;
